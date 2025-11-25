1af48f5b249221de025b625e63c0c04d997d1461
Triwalt <140782140+Triwalt@users.noreply.github.com>
2025-11-25 11:52:58 +0800

    Adapt github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ⚠️ 注意：这里的 '/viterbi-viz/' 必须换成您在 GitHub 上的仓库名称
+  // 例如您的仓库是 https://github.com/user/my-app，这里就填 '/my-app/'
+  base: 'https://github.com/Triwalt/Viewterbi', 
 })
